2e7039225cebe658edb519ec32412ba63f71e84e
PrathnaPel <prathnapel@gmail.com>
2021-03-07 20:56:04 -0500

    Add project files.

---
+++ CFF-CRM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CFF_CRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
